d8984f3a25b3adb56796d75a256a19fffa76299b
liuerfire <liuerfire@gmail.com>
2014-04-23 23:53:05 +0800

    update

--- chapter_01/demo.scm
+++ chapter_01/demo.scm
@@ -4,8 +4,24 @@
 
 (display (* pi (* radius radius)))
 
+(display "\n")
 
 (define (abs x)
   (cond ((> x 0) x)
         ((= x 0) 0)
-        ((< x 0) (-x))))
+        ((< x 0) (- 0 x))))
+
+(display (abs -1))
+
+(display "\n")
+
+(define plus4 (lambda (a) (+ a 4)))
+(display (plus4 4))
+
+(display "\n")
+
+(define x 5)
+(display (+ (let ((x 3))
+             (+ x (* x 10)))
+            x))
+(display "\n")
