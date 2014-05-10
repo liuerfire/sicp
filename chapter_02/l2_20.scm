1c622b712b1925a0ef1967e710df38c3d2176cf7
liuerfire <liuerfire@gmail.com>
2014-05-10 21:10:29 +0800

    add better way

--- chapter_02/l2_20.scm
+++ chapter_02/l2_20.scm
@@ -7,7 +7,14 @@
         (parity-iter (cdr l) r p))))
   (parity-iter (cdr args) (car args) (car args)))
 
-(display (same-parity 1 2 3 4 5 6 7))
+; better way
+(define (same-parity-2 a . l)
+  (let ((ev (remainder a 2)))
+    (filter (lambda (x) (= ev (remainder x 2))) l)))
+
+
+(display (same-parity 1 3 4 5 6 7))
 (newline)
-(display (same-parity 2 3 4 5 6 7))
+
+(display (same-parity-2 1 2 3 4 5 6 7))
 (newline)
