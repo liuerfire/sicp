8c314bf3a118796b29c8d2df214b2e437d2f7b75
liuerfire <liuerfire@gmail.com>
2014-05-14 00:15:19 +0800

    update

---
+++ chapter_02/l2_32.scm
@@ -0,0 +1,8 @@
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
+(display (subsets (list 1 2 3)))
+(newline)
