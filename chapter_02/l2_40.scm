cec2d3bb5e68a114ec388e413fccf1306e5ebb42
liuerfire <liuerfire@gmail.com>
2014-06-04 23:02:16 +0800

    update

--- chapter_02/l2_40.scm
+++ chapter_02/l2_40.scm
@@ -1,8 +1,8 @@
 (load "../common.scm")
 
 (define (enumerate-interval i j)
-  (if (>= i j)
-    (list j)
+  (if (> i j)
+    '()
     (cons i (enumerate-interval (+ 1 i) j))))
 
 (define (flatmap proc seq)
@@ -14,6 +14,11 @@
                   (enumerate-interval 1 (- i 1))))
            (enumerate-interval 2 n)))
 
-
+(display (enumerate-interval 1 5))
+(newline)
+(display (unique-pairs 3))
+(newline)
+(display (unique-pairs 4))
+(newline)
 (display (unique-pairs 5))
 (newline)
