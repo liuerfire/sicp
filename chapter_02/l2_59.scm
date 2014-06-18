62125875c091aa66ba2d8ad17dfd8c48a1b596c8
liuerfire <liuerfire@gmail.com>
2014-06-18 22:45:30 +0800

    update

---
+++ chapter_02/l2_59.scm
@@ -0,0 +1,11 @@
+(define (element-of-set? x set)
+  (cond [(null? set) #f]
+        [(equal? x (car set)) #t]
+        [(else (element-of-set? x (cdr set)))]))
+
+(define (union-set set1 set2)
+  (cond [(and (null? set1) (not (null? set2))) set2]
+        [(and (not (null? set1) (null? set2))) set1]
+        [(element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2)]
+        [else (cons (car set1) (union-set (cdr set1) set2))]))
