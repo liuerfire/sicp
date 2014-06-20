05f4f3daeb9546f1ecafec28032560ab6c20f742
liuerfire <liuerfire@gmail.com>
2014-06-20 21:51:04 +0800

    fix error

--- chapter_02/l2_59.scm
+++ chapter_02/l2_59.scm
@@ -1,11 +1,14 @@
 (define (element-of-set? x set)
   (cond [(null? set) #f]
         [(equal? x (car set)) #t]
-        [(else (element-of-set? x (cdr set)))]))
+        [else (element-of-set? x (cdr set))]))
 
 (define (union-set set1 set2)
   (cond [(and (null? set1) (not (null? set2))) set2]
-        [(and (not (null? set1) (null? set2))) set1]
+        [(and (not (null? set1)) (null? set2)) set1]
         [(element-of-set? (car set1) set2)
          (union-set (cdr set1) set2)]
         [else (cons (car set1) (union-set (cdr set1) set2))]))
+
+(display (union-set '(1 2 3) '(3 4 5)))
+(newline)
