3521ab9873d453725ac22d05f033f7a274720426
liuerfire <liuerfire@gmail.com>
2014-09-04 22:12:25 +0800

    update

---
+++ chapter_03/l3_13.scm
@@ -0,0 +1,17 @@
+(define (last-pair x)
+  (if (null? (cdr x))
+    x
+    (last-pair (cdr x))))
+
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+(display z)
+(newline)
+
+(display (last-pair z))
